4a8c715fd8a21f3090d0d050c45d81288b49ee4a
KilometersFan <milespha@usc.edu>
2019-04-08 18:22:35 -0700

    added logic for authenticator.authenicate() and fixed bugs in updater

--- src/util/Authenticator.js
+++ src/util/Authenticator.js
@@ -1,3 +1,33 @@
 class Authenticator{
-	
+	constructor(){
+
+    }
+    authenticate(username, password){
+        var mysql = require('mysql');
+        var con = mysql.createConnection({
+            host: 'localhost',
+            user: 'root',
+            password: 'root',
+            database: 'events'
+        });
+        con.connect(function(err){
+            if(err) {
+                console.log(err);
+                con.end();
+            }
+            var query = "SELECT UserID, username, accessLevel FROM user WHERE username=" + mysql.escape(username) + " AND password=" + mysql.escape(password);
+            con.query(query, function(err, result){
+                if(err) {
+                    console.log(err);
+                    con.end();
+                }
+                else {
+                    var returnString = result[0].UserID + "," + result[0].username + "," + result[0].accessLevel;
+                    console.log(returnString);
+                    con.end();
+                    return returnString;
+                }
+            });
+        });
+    }
 }
--- src/util/EventUpdater.js
+++ src/util/EventUpdater.js
@@ -8,12 +8,17 @@ class EventUpdater{
             database: 'events'
         });
         con.connect(function(err){
-            if(err) return err;
+            if(err) {
+                console.log(err);
+                con.end();
+            }
             var query = "SELECT TimeStamp FROM action ORDER BY TimeStamp DESC LIMIT 1";
             con.query(query, function(err, result){
+                console.log(result);
                 if(err) console.log(err);
-                this.latestTime = result[0].timestamp;
-                console.log(latestTime);
+                this.latestTime = result[0].TimeStamp;
+                console.log(this.latestTime);
+                con.end()
             });
         });
     }
@@ -26,3 +31,5 @@ class EventUpdater{
         return date.getUTCFullYear() + "-" + twoDigits(1 + date.getUTCMonth()) + "-" + twoDigits(date.getUTCDate()) + " " + twoDigits(date.getUTCHours()) + ":" + twoDigits(date.getUTCMinutes()) + ":" + twoDigits(date.getUTCSeconds());
     };
 }
+
+var eu = new EventUpdater();
