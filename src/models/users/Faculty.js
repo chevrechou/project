604b56234707ca8d1da96c01f184346505358f25
KilometersFan <milespha@usc.edu>
2019-04-09 17:57:42 -0700

    added faculty functionality

--- src/models/users/Faculty.js
+++ src/models/users/Faculty.js
@@ -1,6 +1,8 @@
+const EventManager = require('../../util/EventManager.js');
+const User = require('./User.js');
 class Faculty extends User{
-	constructor(id, username){
-		super(id, username);
+	constructor(id, username, eventManager){
+		super(id, username, eventManager);
 		this.accessLevel = 2;
 	}
 
@@ -10,15 +12,19 @@ class Faculty extends User{
 		// JSON should probably be parsed here instead of the EventManager class
 		// I think the EventManager class should only deal with inputting data to sql and not parsing it first
 		// That applies for all these functions that take in json
+		//this will return a comma separated string of data
+		var eventInfo = "'"+event.Title + "'"+ ',' + event.DateTime + ',' + "'"+event.Location + "'"+',' + "'"+event.Description + "'"+ ',' + event.AccessLevel + ',' + event.UserID;
+		this.eventManager.createEvent(eventInfo);
 	}
 
 	// Edit event
 	editEvent(event){
-
+		var eventInfo = event.Title + ',' + event.DateTime + ','+event.Location + ',' +event.Description + ',' + event.AccessLevel + ',' + event.UserID;
+		this.eventManager.editEvent(event.EventID, eventInfo);
 	}
 
 	// Delete event
-	deleteEvent(event){
-
+	deleteEvent(EventID){
+		this.eventManager.deleteEvent(EventID);
 	}
 }
