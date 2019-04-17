aec8aec2759ba7045568d6afebdfa9e02490458b
Swap <swapnil@usc.edu>
2019-04-16 20:36:30 -0700

    Updated sql init code.

--- src/sql/CreateTables.sql
+++ src/sql/CreateTables.sql
@@ -1,45 +1,40 @@
 USE events;
 CREATE TABLE `action` (
-  `ActionID` int(11) NOT NULL AUTO_INCREMENT,
+  `ActionID` int(11) PRIMARY KEY NOT NULL AUTO_INCREMENT,
   `Type` varchar(255) NOT NULL,
   `EventID` int(11) NOT NULL,
-  `TimeStamp` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  PRIMARY KEY (`ActionID`),
-  KEY `action_ibfk_1` (`EventID`)
-) ENGINE=InnoDB AUTO_INCREMENT=27 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+  `TimeStamp` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE `user` (
+  `UserID` int(11) PRIMARY KEY NOT NULL AUTO_INCREMENT,
+  `Username` varchar(255) NOT NULL,
+  `Password` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `AccessLevel` int(11) NOT NULL
+);
+
 CREATE TABLE `event` (
-  `EventID` int(11) NOT NULL AUTO_INCREMENT,
+  `EventID` int(11) PRIMARY KEY NOT NULL AUTO_INCREMENT,
   `Title` varchar(255) NOT NULL,
   `DateTime` varchar(255) NOT NULL,
   `Location` varchar(255) NOT NULL,
   `Description` varchar(255) DEFAULT NULL,
   `AccessLevel` int(11) NOT NULL,
   `UserID` int(11) NOT NULL,
-  PRIMARY KEY (`EventID`),
-  KEY `UserID` (`UserID`),
-  CONSTRAINT `event_ibfk_1` FOREIGN KEY (`UserID`) REFERENCES `user` (`UserID`)
-) ENGINE=InnoDB AUTO_INCREMENT=19 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+  FOREIGN KEY (`UserID`) REFERENCES `user` (`UserID`)
+);
+
 CREATE TABLE `favorites` (
   `userID` int(11) NOT NULL,
   `eventID` int(11) NOT NULL,
-  KEY `userID` (`userID`),
-  KEY `eventID` (`eventID`),
-  CONSTRAINT `favorites_ibfk_1` FOREIGN KEY (`userID`) REFERENCES `user` (`UserID`),
-  CONSTRAINT `favorites_ibfk_2` FOREIGN KEY (`eventID`) REFERENCES `event` (`EventID`) ON DELETE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+  FOREIGN KEY (`userID`) REFERENCES `user` (`UserID`),
+  FOREIGN KEY (`eventID`) REFERENCES `event` (`EventID`) ON DELETE CASCADE
+);
+
 CREATE TABLE `tag` (
-  `TagID` int(11) NOT NULL AUTO_INCREMENT,
+  `TagID` int(11) PRIMARY KEY NOT NULL AUTO_INCREMENT,
   `Name` varchar(255) NOT NULL,
   `EventID` int(11) DEFAULT NULL,
-  PRIMARY KEY (`TagID`),
-  KEY `EventID` (`EventID`),
-  CONSTRAINT `tag_ibfk_1` FOREIGN KEY (`EventID`) REFERENCES `event` (`EventID`) ON DELETE CASCADE
-) ENGINE=InnoDB AUTO_INCREMENT=15 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-CREATE TABLE `user` (
-  `UserID` int(11) NOT NULL AUTO_INCREMENT,
-  `Username` varchar(255) NOT NULL,
-  `Password` varchar(255) NOT NULL,
-  `email` varchar(255) NOT NULL,
-  `AccessLevel` int(11) NOT NULL,
-  PRIMARY KEY (`UserID`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;user
+  FOREIGN KEY (`EventID`) REFERENCES `event` (`EventID`) ON DELETE CASCADE
+);
